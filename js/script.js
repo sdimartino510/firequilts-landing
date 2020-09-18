1c14ffcf97db173ece22de556a667a18f6364ca2
Sergio Di Martino <sergio@webdevserg.io>
2020-09-18 14:58:56 -0700

    getting form working

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
 $(document).ready(function() {
   $('.emailSubmit').click(function(event) {
     event.preventDefault();
+    var emailAddress = $('#inputEmail').val().trim();
+    console.log(emailAddress);
+    $('#inputEmail').val('')
   });
 });
